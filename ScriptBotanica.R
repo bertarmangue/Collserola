4a08694e0be13cbf40958543e4c75abee88049f4
berta valent <berta.valent01@estudiant.upf.edu>
2021-06-07 15:59:45 +0200

    update file with explanations

--- ScriptBotanica.R
+++ ScriptBotanica.R
@@ -1,6 +1,7 @@
 setwd("C:/Users/berta/collserola")
 data <- read.csv("Collserolabo.csv", sep=";", stringsAsFactors = TRUE)
 
+## Per fer el recompte de la riquesa específica de cada parcel·la, fem la suma de les files corresponents i ho emmagatzemem a les variables "e"):
 mask0 <- data$PARCELA == 0
 dat0 <- data[mask0,]
 e0 <- nrow(dat0)
@@ -26,7 +27,15 @@ mask5 <- data$PARCELA == 5
 dat5 <- data[mask5,]
 e5 <- nrow(dat5)
 
+## Per fer els diferents plots agrupem les diferents dades en x i y:
+
 y <- c(e0, e1, e2, e3, e4, e5)
 x <- c(0, 5, 10, 15, 20, 25)
 
+## Plot corresponent a l'exercici 3, el qual mostra la riquesa específica corresponent a cada parcel·la (i per tant, a les diferents distàncies):
+
 plot(x, y, xlab = "Distància (m)", ylab = "Espècies (nombre)", main= "Riquesa específica en funció a la distància", type = "l")
+
+## Plot corresponent a l'exercici 5, el qual mostra quines espècies apareixen més a prop del camí i quines més lluny:
+
+plot(data$ESPECIE, data$DISTANCIA, xlab= "Espècies (nom)", ylab= "Distància (m)", main= "Espècies segons la distància")
